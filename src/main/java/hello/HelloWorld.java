4d2a740f287281818baa4f2c5b8bf4aad9f9796a
InterstellarDrift <InterstellarDrift@github.com>
2023-01-15 19:33:27 -0500

    more tweaking

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,9 +1,11 @@
 package hello;
 
+import org.xguzm.pathfinding.finders.AStarFinder;
 import org.xguzm.pathfinding.grid.GridCell;
 import org.xguzm.pathfinding.grid.NavigationGrid;
 import org.xguzm.pathfinding.grid.finders.AStarGridFinder;
 import org.xguzm.pathfinding.grid.finders.GridFinderOptions;
+import org.xguzm.pathfinding.grid.finders.ThetaStarGridFinder;
 
 import java.util.List;
 
@@ -23,13 +25,23 @@ public class HelloWorld {
         _cells[i][j] = new GridCell(i,j);
       }
     }
-    _cells[8][9].setWalkable(false);
+//    _cells[8][9].setWalkable(false);
+//    for(int i=30; i<50; i++) {
+//      for(int j=30; j<50;j++){
+//        _cells[i][j].setWalkable(false);
+//      }
+//    }
 //    _cells
     NavigationGrid<GridCell> _navGrid = new NavigationGrid(_cells);
     GridFinderOptions opt = new GridFinderOptions();
     opt.allowDiagonal = true;
-    AStarGridFinder<GridCell> _finder = new AStarGridFinder(GridCell.class, opt);
-    List<GridCell> results = _finder.findPath(0,0,32,100,_navGrid);
+    AStarGridFinder<GridCell> _finderA = new AStarGridFinder(GridCell.class, opt);
+//    ThetaStarGridFinder<GridCell> _finderTheta = new ThetaStarGridFinder(GridCell.class, opt);
+    List<GridCell> results = _finderA.findPath(_cells[0][0], _cells[50][90], _navGrid);
+//    List<GridCell> resultstheta = _finderTheta.findPath(0,0,32,100,_navGrid);
+
     System.out.print(results);
+    System.out.println();
+//    System.out.print(resultstheta);
   }
 }
